3c12932ff86516f06e49e349a495afe69509a2b6
whoisrishi <50065885+whoisrishi@users.noreply.github.com>
2026-06-25 21:30:42 +0530

    Add: stone-game - submission-0

---
+++ Data Structures & Algorithms/stone-game/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        n = len(piles)
+        dp = [[0] * n for _ in range(n)]
+
+        for i in range(n):
+            dp[i][i] = piles[i]
+
+        for length in range(2, n + 1):
+            for l in range(n - length + 1):
+                r = l + length - 1
+                dp[l][r] = max(
+                    piles[l] - dp[l + 1][r],
+                    piles[r] - dp[l][r - 1]
+                )
+
+        return dp[0][n - 1] > 0
