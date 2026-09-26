f49bf3b4212015583cbe3fcd4cb197ace54c6525
whoisrishi <50065885+whoisrishi@users.noreply.github.com>
2026-09-26 21:11:39 +0530

    Add: island-perimeter - submission-0

---
+++ Data Structures & Algorithms/island-perimeter/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def islandPerimeter(self, grid):
+        rows, cols = len(grid), len(grid[0])
+        perimeter = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    perimeter += 4
+
+                    if r > 0 and grid[r - 1][c] == 1:
+                        perimeter -= 2
+
+                    if c > 0 and grid[r][c - 1] == 1:
+                        perimeter -= 2
+
+        return perimeter
